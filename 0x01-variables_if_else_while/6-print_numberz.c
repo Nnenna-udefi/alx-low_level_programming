d071097576184e141fb6419e26cf4945c8fe2f58
Nnenna Udefi <miriamudefi@gmail.com>
2022-10-14 12:57:23 -0700

    putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - prints all single digit numbers of
  * base 10 starting from 0, followed by a new line.
  * Return: 0 if no errors, return non-zero if errors.
@@ -7,7 +7,7 @@
 {
 	int n;
 
-	for(n = 0; n < 10; n++)
+	for(n = 48; n < 58; n++)
 	{
 		putchar (n);
 	}
