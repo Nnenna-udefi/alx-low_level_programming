11af4c1c3c055a79cf3ef76d36169a40c652d763
Nnenna Udefi <miriamudefi@gmail.com>
2022-10-19 13:51:32 -0700

    loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,7 +4,7 @@
  * starting from 1 and 2, followed by a new line
  * Return: Always 0;
  */
-int main (void)
+int main(void)
 {
 	unsigned long int i, j, k, j1, j2, k1, k2;
 
@@ -13,7 +13,7 @@ int main (void)
 
 	printf("%lu", j);
 
-	for (i =1; i < 91; i++)
+	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", k);
 		k = k + j;
@@ -31,7 +31,7 @@ int main (void)
 		printf("%lu", k2 % 1000000000);
 		k1 = k1 + j1;
 		j1 = k1 - j1;
-		k2 = k2 - j2;
+		k2 = k2 + j2;
 		j2 = k2 - j2;
 	}
 
