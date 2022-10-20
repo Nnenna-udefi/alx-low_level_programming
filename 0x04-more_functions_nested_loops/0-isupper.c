65c4df312cfff829168c91f19cf072f8bbc878ab
Nnenna Udefi <miriamudefi@gmail.com>
2022-10-20 09:00:30 -0700

    functons

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - function that checks for uppercase character
+ * @c: the int to be checked
+ * Return: 1 if c is lowercase, otherwise 0
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	return (1);
+	else
+		return (0);
+}
