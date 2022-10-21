027182b7b628c99b8cea41a2270846748f1a1aa2
Nnenna Udefi <miriamudefi@gmail.com>
2022-10-21 09:26:48 -0700

    fix

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /**
  * positive_or_negative - function that prints if integer
@@ -8,6 +11,8 @@
 
 void positive_or_negative(int i)
 {
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
 
 	if (i > 0)
 		printf("%d is positive\n", i);
