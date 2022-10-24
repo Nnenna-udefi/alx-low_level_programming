5a5ca9fb91703ad0e490334ab6c70f38d24246e7
Nnenna Udefi <miriamudefi@gmail.com>
2022-10-24 14:31:46 -0700

    null terminator

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,17 +5,17 @@
  */
 void puts_half(char *str)
 {
-int i, j;
-int len = 0;
-
-for (i = 0; str[i]; i++)
-	len++;
-j = len / 2;
-
-if (len % 2 == 1)
-       j = ((len + 1) / 2);
-
-for (i = j; str[i] != '\0'; i++)
-	_putchar(str[i]);
-_putchar('\n');
+	int i, j;
+	int len = 0;
+	
+	for (i = 0; str[i]; i++)
+		len++;
+	j = len / 2;
+	
+	if (len % 2 == 1)
+		j = ((len + 1) / 2);
+	
+	for (i = j; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
