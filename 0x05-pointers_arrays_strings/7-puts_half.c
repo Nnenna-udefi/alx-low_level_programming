793b1d478013416ae719898c6d0e2065c642cbb0
Nnenna Udefi <miriamudefi@gmail.com>
2022-10-24 14:37:28 -0700

    array

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,10 +11,8 @@ void puts_half(char *str)
 	for (i = 0; str[i]; i++)
 		len++;
 	j = len / 2;
-	
 	if (len % 2 == 1)
 		j = ((len + 1) / 2);
-	
 	for (i = j; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	_putchar('\n');
