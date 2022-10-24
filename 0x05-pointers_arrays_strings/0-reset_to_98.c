02e60097dea8c7c21cadef1d593a9c02e62b752b
Nnenna Udefi <miriamudefi@gmail.com>
2022-10-24 11:13:39 -0700

    strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function that points to an int as parameter and 
+ * reset_to_98 - function that points to an int as parameter and
  * updates the value it points to 98
  * @n: input
  * Return - n
