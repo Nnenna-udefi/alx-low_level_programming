d6e5456cf1e21a26e8187ebd8b16b1f0e8aae784
Nnenna Udefi <miriamudefi@gmail.com>
2022-10-28 13:33:51 -0700

    rot

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -21,7 +21,7 @@ void print_buffer(char *b, int size)
 
 	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-		printf("%08x: %02x%02x", i * 10);
+		printf("%08x: ", i * 10);
 		if (i < size / 10)
 		{
 			printf(b, 9, i);
