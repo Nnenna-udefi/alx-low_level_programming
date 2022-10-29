fec5b6d29e0f9a2e9ce847c28b012699e7a60a8c
Nnenna Udefi <miriamudefi@gmail.com>
2022-10-29 15:34:22 -0700

    encrypt

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,16 +7,11 @@
 char *rot13(char *k)
 {
 	int i;
-	char firstset[] = {"ABCDEFGHIJKLMabcdefghijklm"};
-	char secondset[] = {"NOPQRSTUVWXYZnopqrstuvwxyz"};
 
 	for (i = 0; k[i] != '\0'; i++)
 	{
-		if (k[i] == firstset[i] || k[i] == secondset[i])
-		{
-			firstset[i] += 13;
-			secondset[i] -= 13;
-		}
+		if ((k[i] >= 'A' && k[i] < 'N') || (k[i] >= 'a' && k[i] < 'n'))
+			k[i] += 13;
 	}
 	return (k);
 }
