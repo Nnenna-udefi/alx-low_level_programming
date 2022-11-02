d331fd2411f85ef55322f991500cd2325ad814ab
Nnenna Udefi <miriamudefi@gmail.com>
2022-11-02 09:26:44 -0700

    factorial

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - functionn that returns the length of a string
+ * @s: input
+ * Return: Always 0;
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	if (*s == '\0')
+		return (0);
+
+	i = (1 + _strlen_recursion(s + 1));
+	
+	return (i);
+}
