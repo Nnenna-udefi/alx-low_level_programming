bb8c43c85429925b4446d9fb822a7b1784158a4d
Nnenna Udefi <miriamudefi@gmail.com>
2022-11-02 16:10:50 -0700

    wild

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -24,12 +24,12 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(++s1, ++s2));
 	}
-	else if (*s1 = '*')
+	else if (*s1 == '*')
 	{
 		if (*(s1 + 1) == '*')
 			return (wildcmp(++s1, s2));
 		else
-			return (wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2);
+			return (wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2));
 	}
 	else if (*s2 == '*')
 	{
