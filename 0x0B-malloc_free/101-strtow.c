42beb91a144610fa99e230ac2d4fd337026c7632
Nnenna Udefi <miriamudefi@gmail.com>
2022-11-07 21:05:00 -0800

    str

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * strtow - function that splits a string into two words
+ * @str: pointer string
+ * Return: NULL if str == NULL or str == ""
+ */
+char **strtow(char *str)
+{
+	int i, j;
+	char *a;
+
+	if (str == NULL || str == "")
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == (str[i] - 1)
+				return (NULL);
+				}
+	a = malloc(i * (sizeof(str);
+	if (a == NULL)
+	return (NULL);
+	};
