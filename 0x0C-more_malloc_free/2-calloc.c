2ef088627de2079f1f49cb1ff95eb88538bec9fa
Nnenna Udefi <miriamudefi@gmail.com>
2022-11-10 12:44:53 -0800

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,7 +3,7 @@
  * _calloc - function that allocates memory for an array, using malloc
  * @nmemb: input
  * @size: bytes
- * Retun: pointer
+ * Return: pointer
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
