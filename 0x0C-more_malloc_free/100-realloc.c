1f7e35e4fbbf2c0e0b5c13ef6e2b00d3a44c4e98
Nnenna Udefi <miriamudefi@gmail.com>
2022-11-10 12:36:42 -0800

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,23 +8,29 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *b;
+	char *b, *copy;
 	unsigned int i;
-
-	ptr = malloc(old_size);
-
-	for (i = 0; i < old_size; i++)
+	
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
 	{
-		if (new_size > old_size)
-			b = realloc(void *ptr * new_size);
-		else if (new_size == old_size)
-			return (ptr);
-		else if (ptr == NULL)
-			ptr = b;
-		else if (new_size == 0 && ptr != NULL)
-		{
-			free(ptr);
-			return (NULL);
-		}
+		free(ptr);
+		return (NULL);
 	}
+	if (ptr == NULL)
+	{
+		b = malloc(new_size);
+		if (b == NULL)
+			return (NULL);
+		return (b);
+	}
+
+	b = malloc(new_size);
+		return (NULL);
+	copy = ptr;
+	for (i = 0; i < old_size; i++)
+		b[i] = copy[i];
+	free(ptr);
+	return (b);
 }
