45d72c9806c103537a8a97391ab68fa644a35ef8
Nnenna Udefi <miriamudefi@gmail.com>
2022-11-15 08:12:42 -0800

    macro

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,9 +14,9 @@
 int main(int argc, char *argv[])
 {
 	int mul = 0;
-	int num1, num2;
+	unsigned int num1, num2;
 
-	for (num1 = 0, num2 = 0; (num1 >= 0 && num2 >= 0); num1++, num2++)
+	for (num1 = 0, num2 = 0; num1 < num2; num1++, num2++)
 	{
 		if (num1 % 10 && num2 % 10)
 		{
