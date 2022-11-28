03abc33881889bbd5b58f4e897c3d7382c3a6311
Nnenna Udefi <miriamudefi@gmail.com>
2022-11-28 12:41:31 -0800

    lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,21 +1,22 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * struct_listint_s - sinlgy linked list
+ * struct listint_s - sinlgy linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  *
  */
-typedef struct_listint_s
+typedef struct listint_s
 {
 	int n;
-	struct_listint_s *next;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
