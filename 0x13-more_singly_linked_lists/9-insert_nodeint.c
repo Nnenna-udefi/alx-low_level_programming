0124cf48b348121ce556a4c57c26155f7369f31a
Nnenna Udefi <miriamudefi@gmail.com>
2022-11-30 08:22:46 -0800

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 	}
 	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		free(new);
+		new->next = temp;
+	}
+	new->n = n;
 
 	if (idx == 0)
 	{
