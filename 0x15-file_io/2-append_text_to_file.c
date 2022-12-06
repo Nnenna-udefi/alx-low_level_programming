a3199b10ab0872176c59c4251aa5b5b7a0964f4b
Nnenna Udefi <miriamudefi@gmail.com>
2022-12-06 12:29:37 -0800

    file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,10 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
 	if (text_content == NULL)
-	{
-		close(fd);
 		return (1);
-	}
 	wr = write(fd, text_content, i);
 	if (wr == -1)
 		return (-1);
