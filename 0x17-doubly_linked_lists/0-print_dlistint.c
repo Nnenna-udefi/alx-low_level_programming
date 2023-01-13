20dcdb5213a3e3d82efcb3bea712e539af9ce226
Nnenna Udefi <miriamudefi@gmail.com>
2023-01-13 11:51:02 -0800

    initial commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints all the elements of a dlistint_t list
+ * @h: head
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
