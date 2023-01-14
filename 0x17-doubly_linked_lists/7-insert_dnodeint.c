f1da6330c357a27c91a8b9cc540fac6b5ced785a
Nnenna Udefi <miriamudefi@gmail.com>
2023-01-14 09:38:26 -0800

    add ode

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,6 +18,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
 	if (new == NULL)
 		return (NULL);
 	while (idx != 1)
@@ -27,15 +30,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (temp == NULL)
 			return (NULL);
 	}
+	if (temp->next == NULL)
+		return (add_dnodeint(h, n));
 	new->prev = temp;
 	new->next = temp->next;
 	temp->next->prev = new;
 	temp->next = new;
-
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	if (temp->next == NULL)
-		return (add_dnodeint(h, n));
 
 	return (new);
 }
