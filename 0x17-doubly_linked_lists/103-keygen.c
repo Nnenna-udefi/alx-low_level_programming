e04c5e3eb199fb5f6ad02f6dd86c52dc117fd0f6
Nnenna Udefi <miriamudefi@gmail.com>
2023-01-14 09:23:09 -0800

    password

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Generates and prints passwords for the crackme5 executable
+ * @argc: The number of arguments supplied to the program.
+ * @argv: argument vector
+ * Return: always 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+}
