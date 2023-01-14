bce1241b062d033cca134a34fb7c704d4ae610eb
Nnenna Udefi <miriamudefi@gmail.com>
2023-01-14 09:10:52 -0800

    list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		temp = temp->next;
 		idx--;
+		if (temp == NULL)
+			return (NULL);
 	}
 	new->prev = temp;
 	new->next = temp->next;
