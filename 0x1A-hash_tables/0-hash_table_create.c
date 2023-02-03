fd2a2bf2a207088c513966bc410e7dfe2c980321
Nnenna Udefi <miriamudefi@gmail.com>
2023-02-03 05:16:32 -0800

    hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hash table
+ * @size: size of the array
+ * Return: a pointerr to the newly created has table
+ * or NULL if something is wrong
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	
+	if (table->array == NULL)
+		return NULL;
+	for (i = 0; i < table->size; i ++)
+		table->array[i] = NULL;
+
+	return table;
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+Task to create HashTable and use it to Chain Collision Handling
