7140945ca93d450cd05c4bedbe7f971b048b450c
Nnenna Udefi <miriamudefi@gmail.com>
2023-02-03 05:22:54 -0800

    hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,11 +14,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
-	
 	if (table->array == NULL)
-		return NULL;
-	for (i = 0; i < table->size; i ++)
+		return (NULL);
+	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 
-	return table;
+	return (table);
 }
