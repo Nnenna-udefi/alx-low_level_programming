0060cd66f1c6d037a17c08205f590e34b0570ff3
Nnenna Udefi <miriamudefi@gmail.com>
2023-03-27 13:20:20 -0700

    py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,6 +14,7 @@ def island_perimeter(grid):
     height = len(grid)
     """
     perimeter = 0
+
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
