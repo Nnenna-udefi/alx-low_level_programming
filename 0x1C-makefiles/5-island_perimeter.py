ca914fe3169af53d7be5df58d9b8848bb4a0c901
Nnenna Udefi <miriamudefi@gmail.com>
2023-03-27 13:25:06 -0700

    pys

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines a perimeter island function"""
 
-
 def island_perimeter(grid):
     """Returns the perimeter of the island in grid:
     Args:
@@ -17,10 +16,10 @@ def island_perimeter(grid):
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                # if width & height = 1(land), check left
-                if j == 0 or grid[i][j-1] == 0:
-                    perimeter += 1
+                if grid[i][j] == 1:
+                    # if width & height = 1(land), check left
+                    if j == 0 or grid[i][j-1] == 0:
+                        perimeter += 1
                     # check right
                     if j == len(grid[i])-1 or grid[i][j+1] == 0:
                         perimeter += 1
