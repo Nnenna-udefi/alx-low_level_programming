856ed3ceb2f5f623bc142b02e914fa6f4838ec44
Nnenna Udefi <miriamudefi@gmail.com>
2023-05-08 23:54:05 +0100

    Update 106-linear_skip.c

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -2,38 +2,37 @@
 
 /**
  * linear_skip - searches for a value in a skip list
- *
  * @list: input list
  * @value: value to search in
  * Return: index of the number
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *go;
+	skiplist_t *algo;
 
 	if (list == NULL)
 		return (NULL);
 
-	go = list;
+	algo = list;
 
 	do {
-		list = go;
-		go = go->express;
+		list = algo;
+		algo = algo->express;
 		printf("Value checked at index ");
-		printf("[%d] = [%d]\n", (int)go->index, go->n);
-	} while (go->express && go->n < value);
+		printf("[%d] = [%d]\n", (int)algo->index, algo->n);
+	} while (algo->express && algo->n < value);
 
-	if (go->express == NULL)
+	if (algo->express == NULL)
 	{
-		list = go;
-		while (go->next)
-			go = go->next;
+		list = algo;
+		while (algo->next)
+			algo = algo->next;
 	}
 
 	printf("Value found between indexes ");
-	printf("[%d] and [%d]\n", (int)list->index, (int)go->index);
+	printf("[%d] and [%d]\n", (int)list->index, (int)algo->index);
 
-	while (list != go->next)
+	while (list != algo->next)
 	{
 		printf("Value checked at index [%d] = [%d]\n", (int)list->index, list->n);
 		if (list->n == value)
